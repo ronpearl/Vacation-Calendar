0b0600616d7e6ca1dfd3e996294a132a06659b03
Ron Pearl <ron@ronaldpearl.com>
2015-06-04 12:26:41 -0700

    MySql Files
    updated vacations_oncall MySql dump file to include only week_number
    column filled in.

--- MySql Files/vacations_vacations_oncall.sql
+++ MySql Files/vacations_vacations_oncall.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE  IF NOT EXISTS `vacations` /*!40100 DEFAULT CHARACTER SET utf8 */;
-USE `vacations`;
+CREATE DATABASE  IF NOT EXISTS `gcu_mkt_wp` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `gcu_mkt_wp`;
 -- MySQL dump 10.13  Distrib 5.5.24, for osx10.5 (i386)
 --
 -- Host: DMKTDBVS1001.gcu.edu    Database: gcu_mkt_wp
@@ -30,3 +30,24 @@ CREATE TABLE `vacations_oncall` (
   PRIMARY KEY (`week_number`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `vacations_oncall`
+--
+
+LOCK TABLES `vacations_oncall` WRITE;
+/*!40000 ALTER TABLE `vacations_oncall` DISABLE KEYS */;
+INSERT INTO `vacations_oncall` VALUES (1,0),(2,0),(3,0),(4,0),(5,0),(6,0),(7,0),(8,0),(9,0),(10,0),(11,0),(12,0),(13,0),(14,0),(15,0),(16,0),(17,0),(18,0),(19,0),(20,0),(21,0),(22,0),(23,0),(24,0),(25,0),(26,0),(27,0),(28,0),(29,0),(30,0),(31,0),(32,0),(33,0),(34,0),(35,0),(36,0),(37,0),(38,0),(39,0),(40,0),(41,0),(42,0),(43,0),(44,0),(45,0),(46,0),(47,0),(48,0),(49,0),(50,0),(51,0),(52,0);
+/*!40000 ALTER TABLE `vacations_oncall` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-06-04 12:25:18
